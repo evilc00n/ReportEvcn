c63b27b5394821448ea5619eadac7f5e44b9c061
evilc00n <romanmiigaik@gmail.com>
2024-01-05 17:30:57 +0300

    1 коммит

---
+++ ReportEvcn.Api/Controllers/ReportController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ReportEvcn.Api.Controllers
+{
+    [Route("api/report")]
+    [ApiController]
+    public class ReportController : ControllerBase
+    {
+    }
+}
---
+++ ReportEvcn.Domain/Interfaces/IAuditable.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReportEvcn.Domain.Interfaces
+{
+    public interface IAuditable
+    {
+        public DateTime CreatedAt { get; set; }
+
+        public long CreatedBy { get; set; }
+
+        public DateTime UpdatedAt { get; set; }
+
+        public long UpdatedBy { get; set;}
+
+
+    }
+}
